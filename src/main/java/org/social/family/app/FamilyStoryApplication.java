0c403d9b8bfabb2996a3ed7bef6d5dffd3e8084c
Dhanunjayarao Routu <dhanuroutu@gmail.com>
2022-12-03 17:40:24 +0530

    Initial commit with structure

---
+++ src/main/java/org/social/family/app/FamilyStoryApplication.java
@@ -0,0 +1,13 @@
+package org.social.family.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/social/family/app/FamilyStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.social.family.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
